5dff4ccf766e1d74caa084d767e052e1ef14b1c6
SakilAziz <sakilaziz12.com>
2024-12-19 16:50:14 +0530

    trying something

--- config/db.js
+++ config/db.js
@@ -7,7 +7,6 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectionLimit: process.env.DB_CONNECTION_LIMIT
 });
 
 module.exports = pool;
